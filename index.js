f346b734f0fd1861f25dcdb3e693cf4fe5543272
MiniDigger <admin@minidigger.me>
2019-11-03 10:48:08 +0100

    Switch to new message api

--- index.js
+++ index.js
@@ -11,23 +11,10 @@ Helper.setHome(process.env.THELOUNGE_HOME || Utils.defaultHome()); // TODO shoul
 const shortcutsFile = path.resolve(Helper.getPackagesPath(), "shortcuts.json");
 
 const code = "";
+const red = '04';
 
 let shortcuts = [];
 let thelounge = null;
-
-function sendMessage(text, chan, client) {
-    chan.pushMessage(client.client, new Msg({
-        type: Msg.Type.ERROR, //TODO send "normal" message
-        text: text,
-    }));
-}
-
-function sendErrorMessage(text, chan, client) {
-    chan.pushMessage(client.client, new Msg({
-        type: Msg.Type.ERROR,
-        text: text,
-    }));
-}
 
 function addShortcut(from, to) {
     shortcuts.push({from: from, to: to});
@@ -65,7 +52,7 @@ function loadShortcuts() {
             shortcuts = JSON.parse(data);
             log.info("[Shortcuts] Loaded " + shortcuts.length + " shortcuts from " + shortcutsFile);
             registerShortcuts();
-        } catch(error) {
+        } catch (error) {
             log.error("[Shortcuts] Error while loading shortcuts: " + shortcuts);
             saveShortcuts(); // TODO not sure if this is a good idea
         }
@@ -92,7 +79,7 @@ const runShortcut = {
             // sendMessage("Run shortcut " + code + command + code +" -> " + code + to + code, target.chan, client);
             client.runAsUser(to, target.chan.id);
         } else {
-            sendErrorMessage("Did not find shortcut " + code + command + code +", has it been removed? " +
+            sendErrorMessage("Did not find shortcut " + code + command + code + ", has it been removed? " +
                 "Removed shortcuts get unregistered on your next restart.", target.chan, client)
         }
     },
@@ -102,24 +89,24 @@ const runShortcut = {
 const shortcutCommand = {
     input: function (client, target, command, args) {
         if (args.length === 0) {
-            sendErrorMessage("Usage: /shortcut <add|list|remove>", target.chan, client);
+            client.sendMessage(red + "Usage: /shortcut <add|list|remove>", target.chan);
             return;
         }
         switch (args[0]) {
             case "add":
             case "addf":
                 if (args.length <= 2) {
-                    sendErrorMessage("Usage: /shortcut " + args[0] + " <from> <to>", target.chan, client);
+                    client.sendMessage(red + "Usage: /shortcut " + args[0] + " <from> <to>", target.chan);
                     return;
                 }
 
                 let from = args[1];
                 if (doesShortcutExist(from)) {
                     if (args[0] === "addf") {
-                        sendMessage("Removing old shortcut...", target.chan, client);
+                        client.sendMessage("Removing old shortcut...", target.chan);
                         removeShortcut(from);
                     } else {
-                        sendErrorMessage("Shortcut "+ code + from + code + " does exist, either remove the old one use use " + code + "/shortcut addf <from> <to>" + code, target.chan, client);
+                        client.sendMessage(red + "Shortcut " + code + from + code + " does exist, either remove the old one use use " + code + "/shortcut addf <from> <to>" + code, target.chan);
                         return;
                     }
                 }
@@ -127,34 +114,34 @@ const shortcutCommand = {
                 let to = args.slice(2).join(" ");
                 addShortcut(from, to);
                 thelounge.Commands.add(from, runShortcut); //TODO this doesn't work for completion?
-                sendMessage("Shortcut " + code + from + code + " -> " + code + to + code + " added", target.chan, client);
+                client.sendMessage("Shortcut " + code + from + code + " -> " + code + to + code + " added", target.chan);
                 break;
             case "list":
                 if (shortcuts.length === 0) {
-                    sendErrorMessage("There are no shortcuts defined, use "+ code +"/shortcut add <from> <to>" + code + " to add one.", target.chan, client);
+                    client.sendErrorMessage(red + "There are no shortcuts defined, use " + code + "/shortcut add <from> <to>" + code + " to add one.", target.chan);
                     return;
                 }
-                sendMessage("There are " + code + shortcuts.length + code + " shortcuts configured: ", target.chan, client);
+                client.sendMessage("There are " + code + shortcuts.length + code + " shortcuts configured: ", target.chan);
                 shortcuts.forEach(function (shortcut) {
-                    sendMessage("- " + code + "/" + shortcut.from + code +" -> " + code + shortcut.to + code, target.chan, client);
+                    client.sendMessage("- " + code + "/" + shortcut.from + code + " -> " + code + shortcut.to + code, target.chan);
                 });
                 break;
             case "remove":
                 if (args.length !== 2) {
-                    sendErrorMessage("Usage: /shortcut remove <name>", target.chan, client);
+                    client.sendMessage(red + "Usage: /shortcut remove <name>", target.chan);
                     return;
                 }
 
                 if (!doesShortcutExist(args[1])) {
-                    sendErrorMessage("Shortcut " + code + args[1] + code + " does not exist", target.chan, client);
+                    client.sendMessage(red + "Shortcut " + code + args[1] + code + " does not exist", target.chan);
                 } else {
                     removeShortcut(args[1]);
                     //thelounge.Commands.remove(from); //TODO unregister command
-                    sendMessage("Shortcut " + code + args[1] + code + " has been removed", target.chan, client);
+                    client.sendMessage("Shortcut " + code + args[1] + code + " has been removed", target.chan);
                 }
                 break;
             default:
-                sendErrorMessage("Usage: /shortcut <add|list|remove>", target.chan, client);
+                client.sendMessage("Usage: /shortcut <add|list|remove>", target.chan);
                 break;
         }
     },
